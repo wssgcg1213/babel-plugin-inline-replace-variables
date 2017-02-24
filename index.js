5ea3198cd339c88a643b22c00ba5a21916df9507
Ryan Tsao <rtsao@uber.com>
2017-02-24 12:13:09 -0800

    eliminate destructuring

--- index.js
+++ index.js
@@ -4,31 +4,34 @@
  * @Email i@zeroling.com
  */
 
-module.exports = ({types: t}) => ({
-  visitor: {
-    Identifier(path, state) {
-      if (path.parent.type === 'MemberExpression') {
-        return;
-      }
-      if (path.parent.type === 'ClassMethod') {
-        return;
-      }
-      if (path.isPure()) {
-        return;
-      }
-      if (!state.opts.hasOwnProperty(path.node.name)) {
-        return;
-      }
-      const replacement = state.opts[path.node.name]
-      if (replacement !== undefined) {
-        const type = typeof replacement
-        if (type === 'boolean') {
-          path.replaceWith(t.booleanLiteral(replacement))
-        } else { // treat as string
-          const str = String(replacement)
-          path.replaceWith(t.stringLiteral(str))
+module.exports = (babel) => {
+  const t = babel.types;
+  return {
+    visitor: {
+      Identifier(path, state) {
+        if (path.parent.type === 'MemberExpression') {
+          return;
+        }
+        if (path.parent.type === 'ClassMethod') {
+          return;
+        }
+        if (path.isPure()) {
+          return;
+        }
+        if (!state.opts.hasOwnProperty(path.node.name)) {
+          return;
+        }
+        const replacement = state.opts[path.node.name]
+        if (replacement !== undefined) {
+          const type = typeof replacement
+          if (type === 'boolean') {
+            path.replaceWith(t.booleanLiteral(replacement))
+          } else { // treat as string
+            const str = String(replacement)
+            path.replaceWith(t.stringLiteral(str))
+          }
         }
       }
     }
   }
-});
+};
